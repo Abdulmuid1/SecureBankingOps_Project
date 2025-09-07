59aa2315c6f3f6604cd2eff7328376e2cc15fe97
Abdulmuid <olaniyanabdulmuid@gmail.com>
2025-09-06 23:17:10 -0600

    Set up Terraform architecture

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,23 @@
+# Create resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Create AKS cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "securebankingops"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "securebankingops-rg"
+}
+
+variable "location" {
+  default = "Canada Central"
+}
+
+variable "aks_cluster_name" {
+  default = "securebankingops-aks"
+}
+
+variable "node_count" {
+  default = 2
+}
