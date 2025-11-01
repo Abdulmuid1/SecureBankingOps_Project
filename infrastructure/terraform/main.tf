1a6805e91bd786a6cd0889556a65fcbc2e0416c1
Abdulmuid <olaniyanabdulmuid@gmail.com>
2025-11-01 15:56:57 -0600

    feat(ci-cd): implement full DevSecOps pipeline with strict Trivy scanning, atomic Helm deploys, NodePort AKS integration

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -14,7 +14,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = var.node_count
-    vm_size    = "Standard_D2s_v3"
+    vm_size    = "Standard_B2s"
+    min_count = 1
+    max_count = 1
+    enable_auto_scaling = true
   }
 
   identity {
